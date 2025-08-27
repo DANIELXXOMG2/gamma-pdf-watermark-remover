64547bfacfc0fdd550833140239f881439cf44a1
danielxxomg2 <daniel.bello-h@uniminuto.edu.co>
2025-08-26 22:07:00 -0500

    fix: Correct render.yaml and fix test syntax

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ from httpx import AsyncClient
 from app import app
 
 @pytest.mark.asyncio
-asnyc def test_read_main():
+async def test_read_main():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
